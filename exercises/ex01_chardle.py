37a5848f565a78e966a09fa8787dfe0b50ba9168
Jonathan Dawson <jdawson2@unc.edu>
2023-01-20 17:19:03 -0500

    Finished Ex01 Jonathan Dawson

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,42 +2,38 @@
 
 __author__ = "730474369"
 
-fivchr = str(input("Enter a 5-character word: "))
+fivchr: str = input("Enter a 5-character word: ")
 if len(fivchr) != 5:
     print(" Error: Word must contain 5 characters ")
     exit()
-    
-sinchr = str(input("Enter a single character: "))
+sinchr: str = input("Enter a single character: ")
 if len(sinchr) != 1:
     print(" Error: Character must be a single character. ")
     exit()
-    
 print("Searching for " + sinchr + " in " + fivchr)
+
 chrnum = 0
-if sinchr != fivchr:
-    print ("No instances of " + sinchr + " found in " + fivchr)
     
 if sinchr == fivchr[0]:
     print(sinchr + " found at index 0 ")
     chrnum +=1
-    print(chrnum, " instance of " + sinchr + " found in " + fivchr )
-    
 if sinchr == fivchr[1]:
     print(sinchr + " found at index 1 ")
     chrnum +=1
-    print(chrnum, " instances of " + sinchr + " found in" + fivchr )
-    
 if sinchr == fivchr[2]:
     print(sinchr + " found at index 2 ")
     chrnum +=1
-    print(chrnum, " instances of " + sinchr + " found in " + fivchr )
-    
 if sinchr == fivchr[3]:
     print(sinchr + " found at index 3 ")
     chrnum +=1
-    print(chrnum, "instances of " + sinchr + " found in " + fivchr )
-    
 if sinchr == fivchr[4]:
     print(sinchr + " found at index 4 ")
     chrnum +=1
-    print(chrnum, "instances of " + sinchr + " found in " + fivchr )
+    
+if chrnum == 0:
+    print("No instances of " + sinchr + " found in " + fivchr)
+if chrnum == 1:
+    print(str(chrnum) + " instance of " + sinchr + " found in " + fivchr)
+else:
+    print(str(chrnum) + " instances of " + sinchr + " found in " + fivchr)
+ 
