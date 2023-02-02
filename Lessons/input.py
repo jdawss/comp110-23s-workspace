082f18ae5345fddc2727e7821b07de87e7ffb130
Jonathan Dawson <jdawson2@unc.edu>
2023-02-01 22:22:37 -0500

    progress on exercise 2

--- Lessons/input.py
+++
@@ -1,6 +0,0 @@
-"""Demonstrates asking the user for input"""
-
-user_name: str = input("What is your name? ")
-print("Hello, " + user_name + " good morning! ")
-print("You are so cool and can program so good!")
-
