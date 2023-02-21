2a9f33941baa34882787b597236a3b6a6c199a39
Jonathan Dawson <jdawson2@unc.edu>
2023-02-20 23:08:03 -0500

    Wordle progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,33 +1,33 @@
-"""Creating a wordle that must be done in one shot"""
+"""Creating a wordle that must be done in one shot."""
 
 __author__ = "730474369"
 
-#Defining the secret word variable"
+# Defining the secret word variable"
 the_word: str = "python"
 
-#Defining the index for the word"
+# Defining the index for the word"
 word_idx: int = 0
 
-#Defining the emoji storage
+# Defining the emoji storage
 emoji_store: str = ""
 
-#Defining the variables of the colored boxes
+# Defining the variables of the colored boxes
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-#Defining the variable for the answer input by user
+# Defining the variable for the answer input by user
 six_let: str = input(f"What is your {len(the_word)}-letter guess? ")
 
-#Creating the loop for length of word and giving answers based on input by user
+# Creating the loop for length of word and giving answers based on input by user
 while len(six_let) != len(the_word):
-    six_let: str = (input(f"That was not {len(the_word)} letters! Try again:"))
+    six_let = (input(f"That was not {len(the_word)} letters! Try again:"))
 if six_let == the_word:
     print(" Woo! You got it! ")   
 else:
     print(" Not quite. Play again soon! ") 
     
-#Creating loop and boolean sequences for the output of Green, white, and yellow boxes based on correctness at each index
+# Creating loop and boolean sequences for the output of Green, white, and yellow boxes based on correctness at each index
 while word_idx < len(the_word):
     if six_let[word_idx] == the_word[word_idx]:
         emoji_store += GREEN_BOX
@@ -44,5 +44,5 @@ while word_idx < len(the_word):
             emoji_store += WHITE_BOX
     word_idx += 1
 
-#Printing the emojis
+# Printing the emojis
 print(emoji_store)
